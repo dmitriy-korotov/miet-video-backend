0cc0c54cc61281942c8bd923ca01186d5f451e59
dmitriy-korotov <dmitriy.korotov.76@mail.ru>
2024-03-24 13:22:36 +0300

    Added some tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -16,14 +16,14 @@ def service_source_dir():
 @pytest.fixture(scope='session')
 def initial_data_path(service_source_dir):
     return [
-        service_source_dir / 'postgresql/data',
+        service_source_dir / 'postgres/data/test',
     ]
 
 
 @pytest.fixture(scope='session')
 def pgsql_local(service_source_dir, pgsql_local_create):
     databases = discover.find_schemas(
-        'miet_video_db',
+        'miet_video',
         [service_source_dir.joinpath('postgres/schemas/test')],
     )
     return pgsql_local_create(list(databases.values()))
--- tests/registration_handler/test_registration.py
+++ tests/registration_handler/test_registration.py
@@ -17,32 +17,47 @@ async def test_invalid_user_json(service_client):
 
 
 async def test_request_without_password(service_client):
-    user_data = json.loads('{"password": "2314"}')
+    user_data = json.loads('{"username": "Dima", "password": "2314"}')
     response = await service_client.post('/v1/users', json=user_data)
     assert response.status == 400
     responseJson = json.loads(response.text)
-    assert responseJson["error"]["error_code"] == 2 # user must have 'login' field
+    assert responseJson["error"]["error_code"] == 3 # user must have 'login' field
 
 
 async def test_request_without_password(service_client):
-    user_data = json.loads('{"login": "2314"}')
+    user_data = json.loads('{"username": "Dima", "login": "2314"}')
     response = await service_client.post('/v1/users', json=user_data)
     assert response.status == 400
     responseJson = json.loads(response.text)
-    assert responseJson["error"]["error_code"] == 3 # user must have 'password' field
+    assert responseJson["error"]["error_code"] == 4 # user must have 'password' field
 
 
 async def test_login_is_string_format(service_client):
-    user_data = json.loads('{"login": 2314}')
+    user_data = json.loads('{"username": "Dima", "login": 2314}')
     response = await service_client.post('/v1/users', json=user_data)
     assert response.status == 400
     responseJson = json.loads(response.text)
-    assert responseJson["error"]["error_code"] == 4 # 'login' field must be in string format
+    assert responseJson["error"]["error_code"] == 6 # 'login' field must be in string format
 
 
 async def test_password_is_string_format(service_client):
-    user_data = json.loads('{"login": "233424", "password": 3424}')
+    user_data = json.loads('{"username": "Dima", "login": "233424", "password": 3424}')
     response = await service_client.post('/v1/users', json=user_data)
     assert response.status == 400
     responseJson = json.loads(response.text)
-    assert responseJson["error"]["error_code"] == 5 # 'password' field must be in string format
+    assert responseJson["error"]["error_code"] == 7 # 'password' field must be in string format
+
+
+async def test_success_registration(service_client):
+    user_data = json.loads('{"username": "Dima", "login": "233424", "password": "3424"}')
+    response = await service_client.post('/v1/users', json=user_data)
+    assert response.status == 201
+    assert response.text == '1'
+
+
+@pytest.mark.pgsql('db_test', files=['initial_data.sql'])
+async def test_already_registrated_user(service_client):
+    user_data = json.loads('{"username": "DimaTest1", "login": "123", "password": "3424"}')
+    response = await service_client.post('/v1/users', json=user_data)
+    assert response.status == 403
+    assert response.text == '0'
