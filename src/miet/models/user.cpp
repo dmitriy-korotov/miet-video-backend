54ee5477d5354a1539fab4af34080bf3c9af4b4e
dmitriy-korotov <dmitriy.korotov.76@mail.ru>
2024-03-29 21:49:20 +0300

    Changed user data modles

---
+++ src/miet/models/user.cpp
@@ -0,0 +1,72 @@
+#include "user.hpp"
+
+#include <miet/utils/json.hpp>
+
+
+
+using namespace userver;
+
+namespace miet::models
+{
+    auto UserRegistrationData::SerializeToJson(userver::formats::json::ValueBuilder& json) noexcept -> bool
+    {
+        if (!utils::JsonProcessor::Write(json, "username", username)) {
+            return false;
+        }
+        if (!utils::JsonProcessor::Write(json, "login", login)) {
+            return false;
+        }
+        if (!utils::JsonProcessor::Write(json, "password", password)) {
+            return false;
+        }
+        return true;
+    }
+
+    auto UserRegistrationData::DeserializeFromJson(const userver::formats::json::Value& json) noexcept -> bool
+    {
+        if (!utils::JsonProcessor::Read(json, "username", username)) {
+            return false;
+        }
+        if (!utils::JsonProcessor::Read(json, "login", login)) {
+            return false;
+        }
+        if (!utils::JsonProcessor::Read(json, "password", password)) {
+            return false;
+        }
+        return true;
+    }
+
+    auto UserData::SerializeToJson(userver::formats::json::ValueBuilder& json) noexcept -> bool
+    {
+        if (!UserRegistrationData::SerializeToJson(json)) {
+            return false;
+        }
+        if (!utils::JsonProcessor::Write(json, "user_id", user_id)) {
+            return false;
+        }
+        if (!utils::JsonProcessor::Write(json, "auth_token", auth_token)) {
+            return false;
+        }
+        if (!utils::JsonProcessor::Write(json, "registration_timestamp", registration_timestamp)) {
+            return false;
+        }
+        return true;
+    }
+
+    auto UserData::DeserializeFromJson(const userver::formats::json::Value& json) noexcept -> bool
+    {
+        if (!UserRegistrationData::DeserializeFromJson(json)) {
+            return false;
+        }
+        if (!utils::JsonProcessor::Read(json, "user_id", user_id)) {
+            return false;
+        }
+        if (!utils::JsonProcessor::Read(json, "auth_token", auth_token)) {
+            return false;
+        }
+        if (!utils::JsonProcessor::Read(json, "registration_timestamp", registration_timestamp)) {
+            return false;
+        }
+        return true;
+    }
+}
---
+++ src/miet/models/user.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <userver/formats/json/value.hpp>
+#include <userver/utils/expected.hpp>
+
+#include <string>
+
+
+
+namespace miet::models
+{
+    struct UserRegistrationData
+    {
+        std::string username;
+        std::string login;
+        std::string password;
+
+        bool SerializeToJson(userver::formats::json::ValueBuilder& json) noexcept;
+        bool DeserializeFromJson(const userver::formats::json::Value& json) noexcept;
+
+    };
+
+    struct UserData final : protected UserRegistrationData
+    {
+        std::string user_id;
+        std::string auth_token;
+        std::string registration_timestamp;
+
+        bool SerializeToJson(userver::formats::json::ValueBuilder& json) noexcept;
+        bool DeserializeFromJson(const userver::formats::json::Value& json) noexcept;
+        
+    };
+    
+}
