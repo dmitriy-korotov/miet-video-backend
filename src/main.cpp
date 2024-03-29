b469243b3b1b5ebb0d5b04d15e21da99b276da7b
dmitriy-korotov <dmitriy.korotov.76@mail.ru>
2024-03-29 23:57:00 +0300

    Deleted db_component

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,15 @@
 #include <userver/clients/http/component.hpp>
+#include <userver/clients/dns/component.hpp>
+#include <userver/storages/postgres/component.hpp>
 #include <userver/components/minimal_server_component_list.hpp>
 #include <userver/server/handlers/ping.hpp>
 #include <userver/server/handlers/tests_control.hpp>
 #include <userver/testsuite/testsuite_support.hpp>
 #include <userver/utils/daemon_run.hpp>
 
-#include <miet/db/db_component.hpp>
 #include <miet/clients/orioks_client.hpp>
 #include <miet/handlers/registration_handler.hpp>
+#include <miet/handlers/authorization_handler.hpp>
 
 
 
@@ -15,12 +17,14 @@ int main(int argc, char* argv[]) {
   auto component_list = userver::components::MinimalServerComponentList()
                             .Append<userver::server::handlers::Ping>()
                             .Append<userver::components::TestsuiteSupport>()
+                            .Append<userver::components::Postgres>("postgres-miet-video-db")
+                            .Append<userver::clients::dns::Component>()
                             .Append<userver::components::HttpClient>()
+                            .Append<miet::clients::OrioksClient>()
                             .Append<userver::server::handlers::TestsControl>();
 
-  miet_video::AddPostgresDBComponent(component_list);
-  miet_video::AddOrioksClientComponent(component_list);
-  miet_video::AddRegistrationHandler(component_list);
+  //miet_video::AddRegistrationHandler(component_list);
+  miet_video::AddAuthorizationHandler(component_list);
 
   return userver::utils::DaemonMain(argc, argv, component_list);
 }
