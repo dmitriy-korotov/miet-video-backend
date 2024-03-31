cdcc492ac2982adbf0c7ff2ab584d6fc25e4b19f
dmitriy-korotov <dmitriy.korotov.76@mail.ru>
2024-03-31 10:42:03 +0300

    Added orioks client component

--- src/miet/clients/orioks_client.cpp
+++ src/miet/clients/orioks_client.cpp
@@ -1,51 +1,126 @@
 #include "orioks_client.hpp"
 
 #include <miet/errors/builder.hpp>
-#include <miet/models/user_registration_data.hpp>
+#include <miet/models/user.hpp>
+#include <miet/utils/json.hpp>
 
 #include <userver/crypto/base64.hpp>
 #include <userver/formats/json/parser/parser.hpp>
+#include <userver/yaml_config/merge_schemas.hpp>
 
-#include <filesystem>
+#include <sstream>
 
 
 
-namespace miet_video
+namespace miet::clients
 {
-    auto OrioksClient::AuntificateUser(const std::string& login, const std::string& password) -> utils::expected<AuthToken, std::string>
+    using namespace userver::utils;
+
+namespace
+{
+    auto BuildAuthInfo(const std::string& login, const std::string& password) -> std::string
     {
-        using header_t = std::pair<std::string, std::string>;
+        std::ostringstream result;
+        result << "Basic " << utils::crypto::base64::Base64Encode(login + ":" + password);
+        return result.str();
+    }
 
-        
-        std::string auth_endpount = std::filesystem::path(s_orioks_api) / "api/v1/auth";
-        auto auth = crypto::base64::Base64Encode(login + ":" + password);
-        auto user_agent = "miet-video/0.1 GNU/Linux 22.04-Ubuntu";
+    auto BuildAuthInfo(const OrioksClient::auth_token_t& token) -> std::string
+    {
+        std::ostringstream result;
+        result << "Bearer " << token;
+        return result.str();
+    }
 
-        std::initializer_list<header_t> headers =
-        {
-            { "Accept", "application/json" },
-            { "Authorization", auth },
-            { "User-Agent", user_agent }
-        };
+    auto BuildUrl(const std::string& connection_type, const std::string& hostname, const std::string& path) -> std::string
+    {
+        return connection_type + "://" + hostname + path;
+    }
 
-        const auto response = m_http_client.CreateRequest().get(s_orioks_api.data())
-                                                           .headers(std::move(headers))
-                                                           .timeout(std::chrono::seconds(1))
-                                                           .perform();
+    auto BuildGetRequest(userver::clients::http::Client& http_client,
+                         const std::string& url,
+                         const std::string& auth_info) -> userver::clients::http::Request
+    {
+        return http_client.CreateRequest()
+                          .url(url)
+                          .headers({
+                            {"Accept", "application/json"},
+                            {"Authorization", auth_info}
+                          })
+                          .timeout(std::chrono::seconds(1));
+    }
+}
 
+    auto OrioksClient::GetStaticConfigSchema() -> yaml_config::Schema
+    {
+        return yaml_config::MergeSchemas<components::LoggableComponentBase>(R"(
+            type: object
+            description: orioks client component
+            additionalProperties: false
+            properties:
+                hostname:
+                    type: string
+                    description: orioks hostname
+                connection-type:
+                    type: string
+                    description: http or https protocol
+        )");
+    }
+
+    auto OrioksClient::AuntificateStudent(const std::string& login, const std::string& password) -> expected<auth_token_t, Error>
+    {
+        auto url = BuildUrl(m_connection_type, m_hostname, "api/v1/auth");
+        auto auth_info = BuildAuthInfo(login, password);
+
+        auto response = BuildGetRequest(m_http_client, url, auth_info).perform();
         auto response_body = formats::json::FromString(response->body_view());
 
         if (!response->IsOk()) {
-            return utils::unexpected(BuildError(response->status_code(), response_body["error"].As<std::string>()));
+            if (response->status_code() == userver::clients::http::Status::Unauthorized) {
+                return unexpected(Error::OrioksUserNotFound);
+            } else {
+                return unexpected(Error::CantGetUserToken);
+            }
         }
-
+        if (!response_body.HasMember("token") || !response_body["token"].IsString()) {
+            return unexpected(Error::UnexpectedResponseBody);
+        }
         return response_body["token"].As<std::string>();
     }
 
-
-
-    void AddOrioksClientComponent(userver::components::ComponentList& component_list)
+    auto OrioksClient::GetStudentInfo(const auth_token_t& auth_token) -> expected<models::StudentInfo, Error>
     {
-        component_list.Append<OrioksClient>();
+        auto url = BuildUrl(m_connection_type, m_hostname, "/api/v1/student");
+        auto auth_info = BuildAuthInfo(auth_token);
+
+        auto response = BuildGetRequest(m_http_client, url, auth_info).perform();
+        auto response_body = formats::json::FromString(response->body_view());
+
+        if (!response->IsOk()) {
+            return unexpected(Error::CantGetStudentInfo);
+        }
+        models::StudentInfo student_info;
+        if (!miet::utils::JsonProcessor::Read(response_body, student_info)) {
+            return unexpected(Error::CantParseStudentInfo);
+        }
+        return student_info;
+    }
+
+    auto OrioksClient::GetStudentDisciplines(const auth_token_t& auth_token) -> expected<std::vector<models::StudyDiscipline>, Error>
+    {
+        auto url = BuildUrl(m_connection_type, m_hostname, "/api/v1/student/disciplines");
+        auto auth_info = BuildAuthInfo(auth_token);
+
+        auto response = BuildGetRequest(m_http_client, url, auth_info).perform();
+        auto response_body = formats::json::FromString(response->body_view());
+
+        if (!response->IsOk()) {
+            return unexpected(Error::CantGetStudyDisciplines);
+        }
+        std::vector<models::StudyDiscipline> study_disciplines;
+        if (!miet::utils::JsonProcessor::Read(response_body, study_disciplines)) {
+            return unexpected(Error::CantParseStudyDisciplines);
+        }
+        return study_disciplines;
     }
 }
--- src/miet/clients/orioks_client.hpp
+++ src/miet/clients/orioks_client.hpp
@@ -1,7 +1,9 @@
 #pragma once
 
-#include <userver/components/component_list.hpp>
+#include <miet/models/student.hpp>
+#include <miet/models/study_discipline.hpp>
 
+#include <userver/components/component_list.hpp>
 #include <userver/components/component.hpp>
 #include <userver/components/loggable_component_base.hpp>
 #include <userver/clients/http/component.hpp>
@@ -9,33 +11,51 @@
 
 
 
-namespace miet_video
+namespace miet::clients
 {
     using namespace userver;
+    using namespace userver::utils;
 
     class OrioksClient final : public components::LoggableComponentBase
     {
     public:
 
         static constexpr std::string_view kName = "orioks-client";
-
         static constexpr std::string_view s_orioks_api = "https://orioks.miet.ru";
 
-        using AuthToken = std::string;
+        enum class Error : uint8_t
+        {
+            OrioksUserNotFound,
+            CantGetUserToken,
+            UnexpectedResponseBody,
+            CantGetStudentInfo,
+            CantParseStudentInfo,
+            CantGetStudyDisciplines,
+            CantParseStudyDisciplines,
+            UnknowError,
+            KSize
+        };
+        using auth_token_t = std::string;
 
         OrioksClient(const components::ComponentConfig& config,
                      const components::ComponentContext& component_context)
             : LoggableComponentBase(config, component_context)
             , m_http_client(component_context.FindComponent<components::HttpClient>().GetHttpClient())
+            , m_hostname(config["hostname"].As<std::string>())
+            , m_connection_type(config["connection-type"].As<std::string>())
         { }
 
-        utils::expected<AuthToken, std::string> AuntificateUser(const std::string& login, const std::string& password);
+        static yaml_config::Schema GetStaticConfigSchema();
+
+        expected<auth_token_t, Error> AuntificateStudent(const std::string& login, const std::string& password);
+        expected<models::StudentInfo, Error> GetStudentInfo(const auth_token_t& auth_token);
+        expected<std::vector<models::StudyDiscipline>, Error> GetStudentDisciplines(const auth_token_t& auth_token);
 
     private:
 
-        clients::http::Client& m_http_client;
+        userver::clients::http::Client& m_http_client;
+        std::string m_hostname;
+        std::string m_connection_type;
 
     };
-
-    void AddOrioksClientComponent(userver::components::ComponentList& component_list);
 }
