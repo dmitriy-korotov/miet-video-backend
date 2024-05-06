864a78af371bb472d288c4144170ba138ffc21f8
dmitriy-korotov <dmitriy.korotov.76@mail.ru>
2024-05-06 23:09:50 +0300

    Added upload lecture handler

---
+++ src/miet/handlers/upload_lecture_handler.cpp
@@ -0,0 +1,83 @@
+#include "upload_lecture_handler.hpp"
+
+#include <miet/utils/json.hpp>
+#include <miet/handlers/helpers/helpers.hpp>
+
+#include <miet/models/lecture.hpp>
+
+#include <userver/server/handlers/exceptions.hpp>
+#include <userver/utils/uuid7.hpp>
+
+
+
+namespace miet::handlers
+{
+    static auto FillArgsFromRequest(const server::http::HttpRequest& request) -> UploadLectureHandleArgs
+    {
+        try {
+            auto jsonBody = formats::json::FromString(request.RequestBody());
+            
+            UploadLectureHandleArgs args
+            {
+                .session_token = helpers::GetSessionToken(request)
+            };
+            utils::JsonProcessor::Read(jsonBody, "subject", args.subject);
+            utils::JsonProcessor::Read(jsonBody, "title", args.title);
+            utils::JsonProcessor::Read(jsonBody, "description", args.description);
+            utils::JsonProcessor::Read(jsonBody, "video", args.video_data);
+            utils::JsonProcessor::Read(jsonBody, "preview", args.preview_data);
+
+            return args;
+        } catch (const std::exception& ex) {
+            throw server::handlers::ClientError(
+                server::handlers::InternalMessage(
+                    fmt::format("Bad request data: '{}'", ex.what())
+                ));   
+        }
+    }
+
+    auto DoUploadLectureHandle(const UploadLectureHandleArgs& args, const UploadLectureHandleDeps& deps) -> void
+    {
+        auto user_id_opt = deps.sessions_manager->GetUserIDIfSessionAlive(args.session_token);
+        if (!user_id_opt.has_value()) {
+            throw server::handlers::ExceptionWithCode<server::handlers::HandlerErrorCode::kForbidden>(
+                server::handlers::InternalMessage(
+                        fmt::format("Session lifetime has expired (token = '{}')", args.session_token)));
+        }
+
+        models::VideoUploadData video;
+        video.video_id = userver::utils::generators::GenerateUuidV7();
+        video.title = std::move(args.title);
+        video.description = std::move(args.description);
+        video.video_src = "None";
+        video.preview_src = "None";
+        video.author_id = std::move(user_id_opt).value();
+
+        models::LectureUploadData lecture
+        {
+            .subject = std::move(args.subject),
+            .video = std::move(video)
+        };
+
+        deps.lectures_manager->AddLecture(lecture);
+    }
+
+    auto UploadLectureHandler::HandleRequestThrow(const server::http::HttpRequest& request,
+                                                  server::request::RequestContext&) const -> std::string
+    {
+        helpers::PrepareJsonResponseHeaders(request);
+        return helpers::CallSafeHttpRequestHandler(request, [this, &request]() -> std::string
+        {
+            UploadLectureHandleArgs args = FillArgsFromRequest(request);
+            
+            UploadLectureHandleDeps deps
+            {
+                .sessions_manager = m_sessions_manager,
+                .lectures_manager = m_lectures_manager
+            };
+
+            DoUploadLectureHandle(args, deps);
+            return {};
+        });
+    }
+}
---
+++ src/miet/handlers/upload_lecture_handler.hpp
@@ -0,0 +1,56 @@
+#pragma once
+
+#include <miet/utils/utils.hpp>
+#include <miet/db/managers/postgres/sessions_manager.hpp>
+#include <miet/db/managers/postgres/lectures_manager.hpp>
+
+#include <userver/components/component.hpp>
+#include <userver/server/handlers/http_handler_base.hpp>
+
+
+
+using namespace userver;
+
+namespace miet::handlers 
+{
+    class UploadLectureHandler final : public server::handlers::HttpHandlerBase
+    {
+    public:
+
+        static constexpr std::string_view kName = "upload-lecture-handler";
+
+        UploadLectureHandler(const components::ComponentConfig& config,
+                             const components::ComponentContext& component_context)
+                : HttpHandlerBase(config, component_context)
+                , m_sessions_manager(utils::CreateViewSharedPtr(&component_context.FindComponent<db::managers::pg::SessionsManager>()))
+                , m_lectures_manager(utils::CreateViewSharedPtr(&component_context.FindComponent<db::managers::pg::LecturesManager>()))
+        { }
+
+        std::string HandleRequestThrow(const server::http::HttpRequest& request,
+                                       server::request::RequestContext&) const override;
+
+    private:
+
+        std::shared_ptr<db::managers::SessionsManagerBase> m_sessions_manager;
+        std::shared_ptr<db::managers::LecturesManagerBase> m_lectures_manager;
+
+    };
+
+    struct UploadLectureHandleArgs
+    {
+        std::string session_token;
+        std::string subject;
+        std::string video_data;
+        std::optional<std::string> preview_data;
+        std::string title;
+        std::optional<std::string> description;
+    };
+
+    struct UploadLectureHandleDeps
+    {
+        std::shared_ptr<db::managers::SessionsManagerBase> sessions_manager;
+        std::shared_ptr<db::managers::LecturesManagerBase> lectures_manager;
+    };
+
+    void DoUploadLectureHandle(const UploadLectureHandleArgs& args, const UploadLectureHandleDeps& deps);
+}
